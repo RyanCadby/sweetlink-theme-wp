4b014bc236d2a0875a0a57ef5ae9c8fa623370fa
Ryan Cadby <cadby.2@gmail.com>
2021-08-23 07:21:21 -0600

    sweet animation edits

--- template-business-card.php
+++ template-business-card.php
@@ -9,13 +9,13 @@ get_header();
         <div class="row">
             <div class="col col-12">
                 <a class="btn btn-primary" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/ryan-cadby.vcf">
-                    <i class="fas fa-address-book"></i> <span>Add My Contact Info</span>
+                    <i class="fas fa-address-book"></i> <span>Download My Contact Info</span>
                 </a>
                 <a class="btn btn-primary" href="<?php echo $GLOBALS['local_env'] ?>/portfolio">
                     <i class="fas fa-eye"></i> <span>View Web Design Portfolio</span>
                 </a>
                 <a class="btn btn-primary" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/ryan-cadby.vcf">
-                <i class="fas fa-question"></i> <span>Request A Free Website Consultation</span>
+                    <i class="fas fa-question"></i> <span>Request A Free Website Consultation</span>
                 </a>
             </div>
         </div>
